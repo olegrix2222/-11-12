5afe1ee5866f2dd5fa20ffc4de8b3828601484d1
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-26 07:17:08 +0000

    code 2

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-запити на імпорт 
-від bs4 імпорт BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.olx.ua/d/uk/obyavlenie/prodam-banan-kievskiy-super-karlik-IDC7KUN.html"
-дані = запити. отримати (URL). Текст
+data = requests.get(URL).text
 
-суп = BeautifulSoup(дані, особливості="html.parser")
+soup = BeautifulSoup(data, features="html.parser")
 
-product_name = суп. find_all("h1", {"class":"css-1soizd2 er34gjf0"})
-product_price = суп. find_all("h3", {"class":"css-ddweki er34gjf0"})
-product_description = суп. find_all("div", {"class":"css-bgzo2k er34gjf0"})
-Similar_ads = суп. find_all("div", {"class":"css-1wbpwse"})
+product_name = soup.find_all("h1", {"class":"css-1soizd2 er34gjf0"})
+product_price = soup.find_all("h3", {"class":"css-ddweki er34gjf0"})
+product_description = soup.find_all("div", {"class":"css-bgzo2k er34gjf0"})
+Similar_ads = soup.find_all("div", {"class":"css-1wbpwse"})
 print("Назва продукта -", product_name)
 print("Ціна продукта -", product_price)
 print("Опис продукта -", product_description)
